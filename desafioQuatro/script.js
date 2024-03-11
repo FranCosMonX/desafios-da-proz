0fbd8697bfbcc5f1a3efa0c14d21d10481c24643
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-03-11 20:40:03 -0300

    concluindo uma parte do desafio

---
+++ desafioQuatro/script.js
@@ -0,0 +1,23 @@
+const logo = document.getElementById("logo")
+console.log(logo)
+
+const classes_post_autor = document.getElementsByClassName("post-autor")
+console.log(classes_post_autor)
+
+const post_dois = document.getElementById("post02")
+console.log(post_dois)
+
+const formulario = document.getElementById("formulario")
+console.log(formulario)
+
+const todos_os_posts = document.getElementById("posts")
+console.log(todos_os_posts)
+
+const data_dos_posts = document.getElementsByClassName("post-data")
+console.log(data_dos_posts)
+
+const texto_dos_posts = document.getElementsByClassName("post-texto")
+console.log(texto_dos_posts)
+
+const lista_redes_sociais = document.getElementsByClassName("lista_redes")
+console.log(data_dos_posts)
