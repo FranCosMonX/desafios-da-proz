0c4f3ffd9a3f8c47ba9470068171df2bd6e59022
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-03-27 22:46:59 -0300

    completando o desafio extra da etapa 3 do desafio 9

--- desafioNove/script.js
+++ desafioNove/script.js
@@ -7,56 +7,57 @@ let emailLabel = document.querySelector('label[for="email"]');
 let emailHelper = document.getElementById("email-helper");
 
 // Mostrar popup de campo obrigatório
-const soMostrarPopUp = () => {
-  usernameLabel.classList.add("required-popup")
-}
-const soMostrarPopUpEmail = () => {
-  emailLabel.classList.add("required-popup")
+const mostrarPopUp = (e) => {
+  const id = e.target.id
+  if (id === "username")
+    usernameLabel.classList.add("required-popup")
+  else if (id === "email")
+    emailLabel.classList.add("required-popup")
+
 }
 
 // Ocultar popup de campo obrigatório
-const ocultarPopup = () => {
-  usernameLabel.classList.remove("required-popup")
-}
-const ocultarPopopEmail = () => {
-  emailLabel.classList.remove("required-popup")
+const ocultarPopup = (e) => {
+  const id = e.target.id
+  if (id === "username")
+    usernameLabel.classList.remove("required-popup")
+  else if (id === "email")
+    emailLabel.classList.remove("required-popup")
 }
 
 // Validar valor do input
-function validarUsername(e) {
-  const tamanho = e.target.value;
-  console.log(e)
+function validarCampo(e) {
+  const valor = e.target.value;
+  const id = e.target.id;
 
-  if (tamanho.length < 3) {
-    usernameInput.classList.remove("correct")
-    usernameInput.classList.add("error")
-    usernameHelper.innerText = "minimo 3 caracteres"
-    usernameHelper.classList.add("visible")
-  } else {
-    usernameInput.classList.remove("error")
-    usernameInput.classList.add("correct")
-    usernameHelper.classList.remove("visible")
+  if (id === "username") {
+    if (valor.length < 3) {
+      usernameInput.classList.remove("correct")
+      usernameInput.classList.add("error")
+      usernameHelper.innerText = "minimo 3 caracteres"
+      usernameHelper.classList.add("visible")
+    } else {
+      usernameInput.classList.remove("error")
+      usernameInput.classList.add("correct")
+      usernameHelper.classList.remove("visible")
+    }
+  } else if (id === "email") {
+    if (valor.includes('@') && valor.includes(".com") && valor.length > 4) {
+      emailInput.classList.remove("error")
+      emailInput.classList.add("correct")
+      emailHelper.classList.remove("visible")
+    } else {
+      emailInput.classList.remove("correct")
+      emailInput.classList.add("error")
+      emailHelper.innerText = "Deve ser um email"
+      emailHelper.classList.add("visible")
+    }
   }
 }
 
-function validarEmail(e) {
-  const string = e.target.value
-
-  if (string.includes('@') && string.includes(".com") && string.length > 4) {
-    emailInput.classList.remove("error")
-    emailInput.classList.add("correct")
-    emailHelper.classList.remove("visible")
-  } else {
-    emailInput.classList.remove("correct")
-    emailInput.classList.add("error")
-    emailHelper.innerText = "Deve ser um email"
-    emailHelper.classList.add("visible")
-  }
-}
-
-usernameInput.addEventListener("focus", soMostrarPopUp)
-emailInput.addEventListener("focus", soMostrarPopUpEmail)
+usernameInput.addEventListener("focus", mostrarPopUp)
+emailInput.addEventListener("focus", mostrarPopUp)
 usernameInput.addEventListener("blur", ocultarPopup)
-emailInput.addEventListener("blur", ocultarPopopEmail)
-usernameInput.addEventListener("change", validarUsername)
-emailInput.addEventListener("change", validarEmail)
+emailInput.addEventListener("blur", ocultarPopup)
+usernameInput.addEventListener("change", validarCampo)
+emailInput.addEventListener("change", validarCampo)
