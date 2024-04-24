f2befbd67e10e198c84f65eeb80c32f0823bd2eb
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-04-23 21:04:25 -0300

    corrigindo objetivo do desafio 9

--- atividadeNove/script.js
+++ atividadeNove/script.js
@@ -1,8 +1,8 @@
 function mostra_possibilidades(a, b) {
-  console.log(a + "+" + b + " = " + (a + b));
-  console.log(a + "-" + b + " = " + (a - b));
-  console.log(a + "*" + b + " = " + (a * b));
-  if (b !== 0) console.log(a + "/" + b + " = " + (a / b));
+  console.log(`${a} + ${b} = ${a + b}`);
+  console.log(`${a} - ${b} = ${a - b}`);
+  console.log(`${a} * ${b} = ${a * b}`);
+  if (b !== 0) console.log(`${a} / ${b} = ${a / b}`);
 }
 
 mostra_possibilidades(4, 5);
